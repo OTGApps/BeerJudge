0bf2222d67e2007f628faf532ecf165a237af728
Mark Rickert <mjar81@gmail.com>
2015-10-22 14:48:27 -0600

    Make the SRM analyzer work on iOS 9

--- app/Screens/SRMAnalyzerScreen.rb
+++ app/Screens/SRMAnalyzerScreen.rb
@@ -1,6 +1,7 @@
 class SRMAnalyzer < MasterScreen
   title "SRM Analyzer"
   tab_bar_item item: "tab_eyedropper", title: "Analyzer"
+  stylesheet SRMAnalyzerScreenStylesheet
 
   attr_accessor :live_preview, :still_image_output, :captured_image_preview
 
@@ -9,142 +10,70 @@ class SRMAnalyzer < MasterScreen
     self.navigationController.navigationBar.translucent = false
     self.automaticallyAdjustsScrollViewInsets = false
     self.edgesForExtendedLayout = UIRectEdgeNone
+
+    #Live Preview video screen setup
+    self.live_preview = append!(UIView, :live_preview)
+    self.still_image_output = AVCaptureStillImageOutput.new
+
+    # Camera View Setup
+    @session = AVCaptureSession.alloc.init
+    @session.sessionPreset = AVCaptureSessionPresetLow
+
+    captureVideoPreviewLayer = set_attributes AVCaptureVideoPreviewLayer.alloc.initWithSession(@session), {
+      frame: self.live_preview.frame,
+      background_color: UIColor.blueColor
+    }
+
+    self.live_preview.layer.addSublayer(captureVideoPreviewLayer)
+    device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+    # Try and start the AVCaptureSession
+    error_ptr = Pointer.new(:object)
+    input = AVCaptureDeviceInput.deviceInputWithDevice(device, error:error_ptr)
+    error = error_ptr #De-reference the pointer.
+    if !input
+      # Handle the error appropriately.
+      NSLog("ERROR: trying to open camera: %@", error)
+    else
+      @session.addInput(input)
+      self.still_image_output.setOutputSettings({AVVideoCodecKey: AVVideoCodecJPEG})
+
+      @session.addOutput(still_image_output)
+      @session.startRunning
+    end
+
+    #Create the gradient view.
+    gradient_view_size = self.live_preview.frame.size.width / 3
+    @gradient_view = append!(UIView, :gradient_view)
+
+    # Placeholder for captured image.
+    self.captured_image_preview = append!(UIImageView, :captured_image_preview)
+
+    # Placeholder for average image color.
+    @average_color = append!(UIView, :average_color)
+
+    # Add the target image over top of the live camera view.
+    @target_area = append!(UIImageView, :target_area)
+
+    # Create the red Slider bar on the the gradient view.
+    @slider = append!(UIView, :slider)
+    @slider_constraints = {
+      origin: @gradient_view.frame.origin.y,
+      height: @gradient_view.frame.size.height
+    }
+
+    # Create the button
+    @capture_button = append!(UIButton, :capture_button).on(:tap){ captureNow }
+
+    @color_view_label = append!(UILabel, :color_view_label)
+
+    Crittercism.leaveBreadcrumb("AnalyzerView") unless Device.simulator?
+
   end
 
   def will_appear
     if Device.simulator?
       App.alert("Unfortunately, this feature ONLY works on real devices.")
-    end
-
-    @view_loaded ||= begin
-
-      # UIView Setup
-      view.setBackgroundColor UIColor.whiteColor
-
-      #Live Preview video screen setup
-      video_ratio = 4.0/3.0
-      self.live_preview = add UIView.new, {
-        left: 0,
-        top: 0,
-        width: self.view.size.width,
-        height: self.view.size.width * video_ratio,
-        background_color: UIColor.whiteColor
-      }
-      self.still_image_output = AVCaptureStillImageOutput.new
-
-      # Camera View Setup
-      @session = AVCaptureSession.alloc.init
-      @session.sessionPreset = AVCaptureSessionPresetLow
-
-      captureVideoPreviewLayer = set_attributes AVCaptureVideoPreviewLayer.alloc.initWithSession(@session), {
-        frame: self.live_preview.frame,
-        background_color: UIColor.blueColor
-      }
-
-      self.live_preview.layer.addSublayer(captureVideoPreviewLayer)
-      device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-
-      # Try and start the AVCaptureSession
-      error_ptr = Pointer.new(:object)
-      input = AVCaptureDeviceInput.deviceInputWithDevice(device, error:error_ptr)
-      error = error_ptr #De-reference the pointer.
-      if !input
-        # Handle the error appropriately.
-        NSLog("ERROR: trying to open camera: %@", error)
-      else
-        @session.addInput(input)
-        self.still_image_output.setOutputSettings({AVVideoCodecKey: AVVideoCodecJPEG})
-
-        @session.addOutput(still_image_output)
-        @session.startRunning
-      end
-
-      #Create the gradient view.
-      gradient_view_size = self.live_preview.frame.size.width / 3
-      @gradient_view = add UIView.new, {
-        left: view.frame.size.width-gradient_view_size,
-        top: 0,
-        width: gradient_view_size,
-        height: self.view.frame.size.height - gradient_view_size,
-        background_color: UIColor.whiteColor
-      }
-
-      @gradient = CAGradientLayer.layer
-      @gradient.frame = view.bounds
-      @gradient.colors = SRM.spectrum
-
-      @gradient_view.layer.insertSublayer(@gradient, atIndex:0)
-
-      # Placeholder for captured image.
-      self.captured_image_preview = add UIImageView.new, {
-        left: self.view.frame.size.width - gradient_view_size,
-        top: self.view.frame.size.height - gradient_view_size,
-        width: gradient_view_size,
-        height: gradient_view_size,
-        content_mode: UIViewContentModeScaleAspectFit,
-        background_color: UIColor.whiteColor
-      }
-
-      # Placeholder for average image color.
-      @average_color = add UIView.new, {
-        left: 0,
-        top: self.view.frame.size.height - gradient_view_size,
-        width: self.view.frame.size.width - gradient_view_size,
-        height: gradient_view_size,
-        background_color: UIColor.whiteColor
-      }
-
-      # Add the target image over top of the live camera view.
-      target_image = UIImage.imageNamed("srm_analyzer_target.png")
-      @target_area = add UIImageView.alloc.initWithImage(target_image), {
-        left: (self.live_preview.frame.size.width / 3) - (target_image.size.width / 2),
-        top: (self.live_preview.frame.size.height / 2) - (target_image.size.height),
-        width: target_image.size.width,
-        height: target_image.size.height
-      }
-
-      # Create the red Slider bar on the the gradient view.
-      @slider = add UIView.new, {
-        left: @gradient_view.frame.origin.x,
-        top: @gradient_view.frame.origin.y,
-        width: @gradient_view.frame.size.width,
-        height: 2,
-        background_color: UIColor.redColor
-      }
-      @slider_constraints = {
-        origin: @gradient_view.frame.origin.y,
-        height: @gradient_view.frame.size.height
-      }
-
-      # Create the button
-      capture_image = UIImage.imageNamed("CaptureButton.png")
-      capture_image_pressed = UIImage.imageNamed("CaptureButton.png")
-      @capture_button = add UIButton.buttonWithType(UIButtonTypeCustom), {
-        left: (self.captured_image_preview.frame.size.width / 2) - (capture_image.size.width / 2) + self.captured_image_preview.frame.origin.x,
-        top: (self.captured_image_preview.frame.size.height / 2) - (capture_image.size.height / 2) + self.captured_image_preview.frame.origin.y,
-        width: capture_image.size.width,
-        height: capture_image.size.height
-      }
-      @capture_button.setBackgroundImage(capture_image, forState: UIControlStateNormal)
-      @capture_button.setBackgroundImage(capture_image_pressed, forState: UIControlStateHighlighted)
-
-      @capture_button.when(UIControlEventTouchUpInside) do
-        captureNow
-      end
-
-      # @camera_timer = EM.add_periodic_timer 1.0 do
-      #   captureNow
-      # end
-
-      @color_view_label = add UILabel.new, {
-        frame: @average_color.frame,
-        text: "Calculated SRM: --",
-        font: UIFont.boldSystemFontOfSize(UIFont.systemFontSize),
-        textAlignment: UITextAlignmentCenter,
-        background_color: UIColor.clearColor
-      }
-
-    Crittercism.leaveBreadcrumb("AnalyzerView") unless Device.simulator?
     end
   end
 
---
+++ app/stylesheets/srm_analyzer_screen_stylesheet.rb
@@ -0,0 +1,102 @@
+class SRMAnalyzerScreenStylesheet < ApplicationStylesheet
+
+  def root_view(st)
+    st.background_color = color.white
+  end
+
+  def live_preview(st)
+    video_ratio = 4.0/3.0
+    st.frame = {
+      left: 0,
+      top: 0,
+      width: :full,
+      height: device_width * video_ratio
+    }
+    st.background_color = color.white
+  end
+
+  def gradient_view(st)
+    gradient_view_size = find(:live_preview).frame.width / 3
+    st.frame = {
+      fr: 0,
+      t: 0,
+      fb: 50,
+      width: gradient_view_size
+    }
+    st.background_color = color.white
+    st.background_gradient = {
+      locations: nil,
+      colors: SRM.ui_spectrum
+    }
+  end
+
+  def captured_image_preview(st)
+    gradient_view_size = find(:live_preview).frame.width / 3
+    st.frame = {
+      fl: 0,
+      fb: 50,
+      w: gradient_view_size,
+      h: gradient_view_size
+    }
+    st.content_mode = :scale_aspect_fit
+    st.background_color = color.random
+  end
+
+  def average_color(st)
+    gradient_view_size = find(:live_preview).frame.width / 3
+    st.frame = {
+      left: 0,
+      fb: 50,
+      width: device_width - gradient_view_size,
+      height: gradient_view_size,
+    }
+    st.background_color = color.random
+  end
+
+  def target_area(st)
+    st.image = image.resource("srm_analyzer_target")
+    image_size = 50
+    live_preview = find(:live_preview).frame
+    st.frame = {
+      left: (live_preview.width / 3) - (image_size / 2),
+      top: (live_preview.height / 2) - (image_size),
+      width: image_size,
+      height: image_size
+    }
+  end
+
+  def slider(st)
+    gradient_view = find(:gradient_view)
+    st.frame = {
+      left: gradient_view.frame.x,
+      top: gradient_view.frame.y,
+      width: gradient_view.frame.width,
+      height: 2,
+    }
+    st.background_color = color.red
+  end
+
+  def capture_button(st)
+    captured_image_preview = find(:captured_image_preview)
+    image_size = 75
+
+    st.frame = {
+      fr: (captured_image_preview.frame.width / 2) - (image_size / 2) + captured_image_preview.frame.x,
+      top: (captured_image_preview.frame.height / 2) - (image_size / 2) + captured_image_preview.frame.y,
+      width: image_size,
+      height: image_size,
+    }
+
+    st.background_image_normal = image.resource("CaptureButton")
+    st.background_image_highlighted = image.resource("CaptureButton")
+  end
+
+  def color_view_label(st)
+    st.frame = find(:average_color).frame
+    st.text = "Calculated SRM: --"
+    st.font = UIFont.boldSystemFontOfSize(UIFont.systemFontSize)
+    st.text_alignment = :center
+    st.background_color = color.clear
+  end
+
+end
